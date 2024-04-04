5fa0c41d12e28d77bfd5b209c1b91e629a91391a
Apolinario8 <goncalocardoso2016@gmail.com>
2024-04-04 21:10:24 +0100

    methane producing pathways

--- scripts/search_ec_in_genome.py
+++ scripts/search_ec_in_genome.py
@@ -2,8 +2,8 @@ from genome_annotation import FunctionalAnnotation
 
 
 def main():
-    genome_annotation = FunctionalAnnotation(r"C:\Users\gonca\OneDrive\Documentos\GitHub\UC_projeto\DB")
-    genome_annotation.identify_gene_by_homology_from_ec("blastp", "3.5.99.2")
+    genome_annotation = FunctionalAnnotation(r"C:\Users\gonca\Documents\GitHub\UC_projeto\DB") 
+    genome_annotation.identify_gene_by_homology_from_ec("blastp", "2.4.2.1")
 
 
 
